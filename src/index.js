bf2854729b0706816c4be77933801c1ee89c020f
Ekaterina Silchenko <maksil.17@gmail.com>
2022-11-07 03:07:50 +0200

    add commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-import './css/styles.css';
+import './js/createMarkup';
 
-const button = document.querySelector('.dropbtn');
-const dropdownContent = document.querySelector('.dropdown-content');
-const dropdown = document.querySelector('.dropdown');
+import './js/pagination';
 
-window.addEventListener('click', onClick);
+import './js/searchMovies';
 
-function onClick(event) {
-  dropdownContent.classList.toggle('show');
-  if (!event.target.classList.contains('dropbtn')) {
-    const dropdowns = document.getElementsByClassName('dropdown-content');
-    console.log(dropdowns);
+import './js/modalTeam';
 
-    for (let i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      console.log(openDropdown);
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+import './js/movieCardModalAndriy';
+
+import './js/filterGenreMainPage';
