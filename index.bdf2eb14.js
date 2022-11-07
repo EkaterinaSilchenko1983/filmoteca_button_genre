3731c8e7cddc4e2a136e91c00ec912b110072942
EkaterinaSilchenko1983 <105131083+EkaterinaSilchenko1983@users.noreply.github.com>
2022-11-07 01:08:29 +0000

    Deploying to gh-pages from @ EkaterinaSilchenko1983/filmoteca_button_genre@bf2854729b0706816c4be77933801c1ee89c020f 🚀

--- index.bdf2eb14.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".dropbtn");const o=document.querySelector(".dropdown-content");document.querySelector(".dropdown");window.addEventListener("click",(function(t){if(o.classList.toggle("show"),!t.target.classList.contains("dropbtn")){const o=document.getElementsByClassName("dropdown-content");console.log(o);for(let t=0;t<o.length;t++){let e=o[t];console.log(e),e.classList.contains("show")&&e.classList.remove("show")}}}));
-//# sourceMappingURL=index.bdf2eb14.js.map
